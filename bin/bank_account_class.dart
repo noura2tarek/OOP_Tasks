0ef2297244c56268d077ce0fabcdf1d7b0650c47
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2024-12-22 21:18:08 +0200

    Edit bank account class

--- bin/bank_account_class.dart
+++ bin/bank_account_class.dart
@@ -1,37 +1,37 @@
 class BankAccount {
   //Fields
-  String accountId = '';
-  double balance = 0;
+  final String _accountId ;
+  double _balance;
 
   // Constructors
-  // Constructor: initializes accountID with given amount and initializes balance = 0
-  BankAccount(this.accountId) : balance = 0;
+  // Constructor: initializes __accountId with given amount and initializes _balance = 0
+  BankAccount(this._accountId) : _balance = 0;
 
-  // Named constructor: initializes accountID and sets balance to the specified amount
-  BankAccount.withBalance(this.accountId, this.balance);
+  // Named constructor: initializes __accountId and sets _balance to the specified amount
+  BankAccount.withBalance(this._accountId, this._balance);
 
   // Functions
   // Deposit function
   void deposit(double amount) {
-    balance += amount;
+    _balance += amount;
   }
 
   // Withdraw function
   void withdraw(double amount) {
     if (amount < 0) {
       print('Error occurred-> amount cannot be negative');
-    } else if (amount > balance) {
-      print('Error occurred-> insufficient balance');
+    } else if (amount > _balance) {
+      print('Error occurred-> insufficient _balance');
     } else {
-      balance -= amount;
+      _balance -= amount;
       print("Withdrawal of \$$amount successful.");
     }
   }
 
   // Display function
   void displayAccountInfo() {
-    print('The Account Id is: $accountId');
-    print('The current Balance is: \$$balance');
+    print('The Account Id is: $_accountId');
+    print('The current _balance is: \$$_balance');
     print('-------------------');
   }
 }
